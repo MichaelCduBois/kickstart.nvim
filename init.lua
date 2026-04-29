85fea767a2b7b38aece42c42de23df876a2c0c8d
Michael duBois <mdubois@leagueify.local>
2026-04-29 14:26:13 -0600

    Configure LSPs

--- init.lua
+++ init.lua
@@ -603,18 +603,15 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       ---@type table<string, vim.lsp.Config>
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-
-        stylua = {}, -- Used to format Lua code
+        basedpyright = {},
+        biome = {},
+        emmet_language_server = {},
+        gopls = {},
+        marksman = {},
+        rust_analyzer = {},
+        stylua = {},
+        tailwindcss = {},
+        vtsls = {},
 
         -- Special Lua Config, as recommended by neovim help docs
         lua_ls = {
@@ -670,6 +667,8 @@ require('lazy').setup({
         vim.lsp.enable(name)
       end
     end,
+
+    vim.lsp.document_color.enable(true, nil, { style = "virtual" })
   },
 
   { -- Autoformat
